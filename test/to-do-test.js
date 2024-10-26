e8b6ce73df82c7049d5b3f6fb61b06c56e971efb
sallybss <salbse01@easv365.dk>
2024-10-26 02:31:42 +0200

    test setup (not ready)

--- test/to-do-test.js
+++ test/to-do-test.js
@@ -3,8 +3,3 @@ import { Selector } from 'testcafe';
 fixture`My TO DO test`
     .page`https://test.sallybss.dk/todo/`;
 
-test('My Test', async t => {
-    await t
-        .click(Selector('#someElement'))
-        .expect(Selector('#otherElement').innerText).eql('Expected Text');
-});
